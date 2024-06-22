a5cb3e07c1a7bd932e926aca770e0c181e130aca
aqmuratov <mirbekaqmuratov@gmail.com>
2024-06-22 12:36:07 +0500

    admin panel ozgerttirildi

--- blog/admin.py
+++ blog/admin.py
@@ -1,5 +1,9 @@
 from django.contrib import admin
 from . import models
-# Register your models here.
 
-admin.site.register(models.Blog)
+# Register your models here.
+class BlogAdmin(admin.ModelAdmin):
+    list_display = ['id','title','image','created','updated']
+    list_display_links=['title','id']
+
+admin.site.register(models.Blog,BlogAdmin)
